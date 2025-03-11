fe5cd6e84f808e9ababcd4f9cbb2b57f16b3796e
Masion-coder <2408766306@qq.com>
2025-03-11 22:21:33 +0800

    常见组件-表格组件&分页组

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import "element-plus/dist/index.css";
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App)
+    .use(ElementPlus, {
+        locale: zhCn,
+    })
+    .mount("#app");
--- src/views/ElementDemo.vue
+++ src/views/ElementDemo.vue
@@ -1,5 +1,40 @@
 <script setup>
+import { ref } from "vue"
+const tableData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+]
 
+const currentPage = ref(1)
+const pageSize = ref(10)
+const total = ref(40)
+const size = ref('default')
+const background = ref(true) // 是否显示背景色
+
+const handleSizeChange = (val) => {
+    console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val) => {
+    console.log(`current page: ${val}`)
+}
 </script>
 
 <template>
@@ -20,10 +55,32 @@
         <el-button type="warning" plain>Warning</el-button>
         <el-button type="danger" plain>Danger</el-button>
     </div>
+
+    <!-- 表格 -->
+    <div class="mb-4">
+        <el-table :data="tableData" stripe style="width: 100%">
+            <el-table-column prop="date" label="Date" width="180" align="center" />
+            <el-table-column prop="name" label="Name" width="180" />
+            <el-table-column prop="address" label="Address" />
+        </el-table>
+    </div>
+
+    <!-- //分页条 -->
+    <el-pagination
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="[10, 20, 30, 40]"
+        :size="size"
+        :background="background"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+    />
 </template>
 
 <style scoped>
-    .mb-4 {
-        margin-bottom: 10px;
-    }
+.mb-4 {
+    margin-bottom: 10px;
+}
 </style>
